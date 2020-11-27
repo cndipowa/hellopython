7b521bbc7ed5d20e22071df5cf3abfa91599571f
che <cndipowa@users.noreply.github.com>
2020-11-27 08:27:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
 setup(
 name='hellopython',
 version='0.0.1',
 description='Say Hello',
+long_description=long_description,
+long_description_content_type="text/markdown",
 py_modules=["hellopython"],
 package_dir={'':'src'},
 )
